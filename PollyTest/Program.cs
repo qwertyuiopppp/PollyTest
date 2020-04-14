1c0ea558077730dc174f2f24b9f7cd4210fae824
zhangjiaming <1169499932@qq.com>
2020-04-14 15:43:36 +0800

    增加根据返回结果的重试例子

--- PollyTest/Program.cs
+++ PollyTest/Program.cs
@@ -7,30 +7,57 @@ namespace PollyTest
 {
 	class Program
 	{
-        private static void Main(string[] args)
-        {
-            var policy = Policy
-              .Handle<Exception>()
-              .RetryAsync(2, async (exception, retryCount) =>
-              {
-                  Console.WriteLine("333333:" + exception.Message + "------" + $"第{retryCount}次重试");
-              });
+		private static void Main(string[] args)
+		{
 
 
-            var result = policy.ExecuteAsync(() => Test());
-            Console.WriteLine("444444:");
-            Console.ReadKey();
-        }
+			ResultReTry();
+			Console.ReadKey();
+		}
 
-        private static async Task Test()
-        {
-            //Convert.ToInt32("w");
-            using (var httpClient = new HttpClient())
-            {
-                var response = httpClient.GetAsync("http://news.cnblogs.com/Category/GetCategoryList?bigCateId=11&loadType=0").Result;
-                var s = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("111111:" + s);
-            }
-        }
-    }
+
+		private static void ResultReTry()
+		{
+			var policy = Policy
+				  .HandleResult<bool>((r) => r==false)
+				  .RetryAsync(2, async (exception, retryCount) =>
+				  {
+					  Console.WriteLine("333333:" + exception.ToString() + "------" + $"第{retryCount}次重试");
+
+				  });
+			var result = policy.ExecuteAsync(() => T());
+			Console.WriteLine("444444:");
+		}
+
+		private static Task<bool> T()
+		{
+			return Task.FromResult(true);
+		}
+
+		private static void ExceptionReTry()
+		{
+			var policy = Policy
+			  .Handle<Exception>()
+			  .RetryAsync(2, async (exception, retryCount) =>
+			  {
+				  Console.WriteLine("333333:" + exception.Message + "------" + $"第{retryCount}次重试");
+			  });
+
+			var result = policy.ExecuteAsync(() => Test());
+			Console.WriteLine("444444:");
+		}
+
+		private static async Task Test()
+		{
+			//Convert.ToInt32("w");
+			using (var httpClient = new HttpClient())
+			{
+				var response = httpClient.GetAsync("http://news.cnblogs.com/Category/GetCategoryList?bigCateId=11&loadType=0").Result;
+				var s = await response.Content.ReadAsStringAsync();
+				Console.WriteLine("111111:" + s);
+			}
+		}
+
+
+	}
 }
